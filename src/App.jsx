9eaa55d6255c2bb5ae1ad49e4b1cf25a75830e00
ruthyi <jbruthlizcano@gmail.com>
2022-12-09 16:50:57 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import CharacterList from './components/CharacterList';
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="bg-dark text-white ">
+      <h1 className='text-center display-1 py-4'>Rick and Morty</h1>
+      <CharacterList/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/CharacterList.jsx
@@ -0,0 +1,67 @@
+import { useEffect, useState } from 'react';
+import Character from './Character';
+
+function NavPage(props){
+    return(
+        <header className='d-flex justify-content-between align-items-center'>
+            <p>
+                Page: {props.page}
+            </p>
+            <button className='btn btn-primary btn-sm'
+             onClick={()=> props.setPage(props.page+1)}
+            >
+                Page {props.page+1}
+            </button>
+        </header>
+    );
+}
+function CharacterList() {
+    const [characters, setCharacter] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [page, setPage] = useState(1);
+
+    useEffect(() => {
+        //Realiza la peticion a otro servidor de forma asincronica
+        async function fetchData() {
+            const response = await fetch(`https://rickandmortyapi.com/api/character?page=${page}`);
+            const data = await response.json(); //Convierte la respuesta en un archivo json
+            // console.log(data.results);
+            setLoading(false);
+            setCharacter(data.results);
+            // console.log(characters);
+        }
+
+        fetchData();
+    }, [page]);
+
+   
+
+    return (
+       
+        <div className='container ' >
+
+        <NavPage page={page} setPage={setPage}/>
+
+            { loading ?(<h1>Loading..</h1>):(
+                <div className='row'>
+                {
+                    characters.map(character => {
+                        return (
+                            <div className='col-md-4' key={character.id}>
+                                <Character  character={character}/>
+                            </div>
+                            
+                        )
+                    })
+                }
+                </div>
+            )
+                 
+            }
+           
+           <NavPage page={page} setPage={setPage}/> 
+        </div>
+
+    )
+}
+export default CharacterList;
